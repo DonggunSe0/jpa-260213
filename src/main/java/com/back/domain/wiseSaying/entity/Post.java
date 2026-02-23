3627fe22e406ea77b4d2433fb52128d68add301c
DonggunSe0 <cloudhenry1@gmail.com>
2026-02-23 15:10:47 +0900

    15 NoArgsConsturctor

--- src/main/java/com/back/domain/wiseSaying/entity/Post.java
+++ src/main/java/com/back/domain/wiseSaying/entity/Post.java
@@ -2,24 +2,24 @@ package com.back.domain.wiseSaying.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Setter
 @Getter
-@RequiredArgsConstructor
+@NoArgsConstructor
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private final String title;
+    private String title;
     @Column(columnDefinition = "TEXT")
-    private final String content ;
+    private String content ;
 
-    public Post() {
-        this.title = "";
-        this.content = "";
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 
 }
